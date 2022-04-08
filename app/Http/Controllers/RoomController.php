ef974430f480577697434b634c99af7b08b387bd
alfathabibi <alfath.abibi@gmail.com>
2022-04-08 14:44:29 +0800

    tambah upload foto kamar

--- app/Http/Controllers/RoomController.php
+++ app/Http/Controllers/RoomController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Room;
+use App\Models\RoomPhotos;
 use Illuminate\Http\Request;
 
 class RoomController extends Controller
@@ -25,7 +26,8 @@ class RoomController extends Controller
     }
 
     public function edit($room_number){
-        $room = Room::where('room_number', $room_number)->firstOrFail();
+        $room = Room::where('room_number', $room_number)->with('photos')->firstOrFail();
+        
         return view('pages/rooms/updateroom',[
             "title" => "Update Room",
             'active' => 'rooms',
@@ -34,6 +36,7 @@ class RoomController extends Controller
     }
 
     public function store(Request $request){
+
         $validatedData = $request->validate([
             'room_number' => 'required|unique:rooms|max:4',
             'price' => 'required',
@@ -42,7 +45,27 @@ class RoomController extends Controller
             'floor' => 'required|max:3'
         ]);
 
-        Room::create($validatedData);
+        $validatedPhotos = $request->validate([
+            'photo1' => 'image|file|max:2048',
+            'photo2' => 'image|file|max:2048',
+            'photo3' => 'image|file|max:2048',
+        ]);
+
+        $photo1 = $request->file('photo1')->store('room-photos');
+        $photo2 = $request->file('photo2')->store('room-photos');
+        $photo3 = $request->file('photo3')->store('room-photos');
+
+        $room = Room::create($validatedData)->photos()->createMany([
+            [
+                "text" => $photo1
+            ],
+            [
+                "text" => $photo2
+            ],
+            [
+                "text" => $photo3
+            ]
+        ]);
 
         $request->session()->flash('success', 'Add Room Successfull!');
 
@@ -54,7 +77,9 @@ class RoomController extends Controller
             'room_number' => 'required'
         ]);
 
-        $room = Room::where('room_number', $validatedData['room_number'])->delete();
+        $room = Room::where('room_number', $validatedData['room_number'])->firstOrFail();
+        RoomPhotos::where('room_id', $room->id)->delete();
+        $room->delete();
         
         $request->session()->flash('success', 'Delete Room Successfully');
         
@@ -62,6 +87,7 @@ class RoomController extends Controller
     }
 
     public function update(Request $request, $room_number){
+
         $validatedData = $request->validate([
             'room_number' => 'required|max:4',
             'price' => 'required',
@@ -69,6 +95,19 @@ class RoomController extends Controller
             'room_area' => 'required|max:2',
             'floor' => 'required|max:3'
         ]);
+
+        if($request->file('photo1')){
+            $photo1 = $request->file('photo1')->store('room-photos');
+            RoomPhotos::find($request['idPhoto1'])->update([
+                "text" => $photo1
+            ]);
+        }
+        // if($request->file('photo1')){
+            
+        // }
+        // if($request->file('photo1')){
+            
+        // }
 
         $updatedRoom = Room::where('room_number', $room_number)
             ->update($validatedData);
--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,12 +2,23 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Models\RoomPhotos;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Room extends Model
 {
     use HasFactory;
 
     protected $guarded = ['id'];
+
+    public function photos(){
+        return $this->hasMany(RoomPhotos::class);
+    }
+
+    public function delete(){
+        $this->photos()->delete();
+        
+        return parent::delete();
+    }
 }
